545f47891fe78ef525c23405c88cf664b9d48a30
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2023-04-01 14:11:02 +0100

    Initial Commit

---
+++ SFB/SFBApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFBApp.swift
+//  SFB
+//
+//  Created by Amir Diafi on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct SFBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
